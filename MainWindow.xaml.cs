84187266208b34fd4858553a5ecf75c138b1d3fb
Tom Vande Wiele <tvdewiel@hotmail.com>
2024-11-27 09:44:56 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppAppConfig
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string connectionString;
+        public string host;
+        public MainWindow()
+        {
+            InitializeComponent();
+            host = ConfigurationManager.AppSettings["host"];
+            connectionString = ConfigurationManager.ConnectionStrings[host].ConnectionString;         
+            UserTextBox.Text = host;
+            ConnTextBox.Text = connectionString;
+        }
+    }
+}
